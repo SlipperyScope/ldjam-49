73fc55e9e7d889831b9fdaf90673b16d757b6301
Andrew <pXn.Tilde@protonmail.com>
2021-10-02 12:50:38 -0700

    Limited turret rotation speed

--- game/AndrewDev/Turret.cs
+++ game/AndrewDev/Turret.cs
@@ -3,8 +3,18 @@ using System;
 
 public class Turret : Node2D
 {
+    const Boolean SlowMo = false;
+    /// <summary>
+    /// Speed of turret rotation in angles per second
+    /// </summary>
+    [Export]
+    public Single RotationSpeed = 20f;
+
     public Node2D Base { get; private set; }
     public Node2D Gun { get; private set; }
+    private Label Output;
+
+    private Int32 debugCount = 0;
 
     /// <summary>
     /// Ready
@@ -13,6 +23,7 @@ public class Turret : Node2D
     {
         Base = GetNode<Node2D>("Base");
         Gun = GetNode<Node2D>("Gun");
+        Output = GetNode<Label>("/root/Andrew/Label");
     }
 
     /// <summary>
@@ -20,15 +31,34 @@ public class Turret : Node2D
     /// </summary>
     public override void _Process(Single delta)
     {
-        Aim();
+        if (SlowMo && debugCount++ % 30 != 0) return;
+
+        Aim(delta);
         Fire();
     }
 
-    private void Aim()
+    private void Aim(Single delta, Boolean LimitRotationSpeed = true)
     {
         var target = GetTarget();
 
-        Gun.LookAt(target);
+        if (LimitRotationSpeed is true)
+        {
+            var angle = Gun.GetAngleTo(target);
+            var frameRotation = RotationSpeed * delta;
+            //Output.Text = $"Angle: {angle} frameSpeed: {frameRotation} Willhit: {Math.Abs(angle) < frameRotation} gunRot: {Gun.Rotation} nextRot: {(angle < 0f ? frameRotation : -frameRotation)}";
+            if (Math.Abs(angle) < frameRotation)
+            {
+                Gun.LookAt(target);
+            }
+            else
+            {
+                Gun.Rotation -= angle < 0f ? frameRotation : -frameRotation;
+            }
+        }
+        else
+        {
+            Gun.LookAt(target);
+        }
     }
 
     private Vector2 GetTarget()
