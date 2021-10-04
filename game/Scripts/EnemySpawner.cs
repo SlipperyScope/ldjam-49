6c94ce0bcf3193d4b6a20d188d5c6ff7f7c012da
Andrew <pXn.Tilde@protonmail.com>
2021-10-04 00:29:34 -0700

    Integrated spawner with director

--- game/Scripts/EnemySpawner.cs
+++ game/Scripts/EnemySpawner.cs
@@ -14,6 +14,14 @@ public class EnemySpawner : Node
     private PackedScene Spider;
     private PackedScene Truck;
     private PackedScene Car;
+
+    private PackedScene SceneFromPath(String path) => path switch
+    {
+        SpiderPath => Spider,
+        TruckPath => Truck,
+        CarPath => Car,
+        _ => throw new NotImplementedException("Enemy type does not exist")
+    };
     #endregion
 
     [Export]
@@ -23,19 +31,24 @@ public class EnemySpawner : Node
     private NodePath TurretPath;
     private TurretController Turret;
 
+    [Export]
+    private NodePath DirectorPath;
+    private Director Director;
+
     private Rect2 MapBounds;
 
 
-    private Single Time = -1f;
+    //private Single Time = -1f;
 
-    private Single NextSpawnTime;
+    //private Single NextSpawnTime;
 
-    public Int32 Difficulty = 0;
+    //public Int32 Difficulty = 0;
 
     public override void _EnterTree()
     {
         Spider = GD.Load<PackedScene>(SpiderPath);
         Car = GD.Load<PackedScene>(CarPath);
+        Truck = GD.Load<PackedScene>(TruckPath);
     }
 
     public override void _Ready()
@@ -43,48 +56,60 @@ public class EnemySpawner : Node
         MapBounds = GetNode<Sprite>(BackgroundPath).GetRect();
         Turret = GetNode<TurretController>(TurretPath);
         Global = GetNode<GlobalData>(GlobalDataPath);
-        Global.KillIncreased += OnKillIncreased;
+        Director = GetNode<Director>(DirectorPath);
+        Director.EnemyWave += OnEnemyWave;
+        //Global.KillIncreased += OnKillIncreased;
     }
 
     public override void _Process(Single delta)
     {
-        Time += delta;
+        //Time += delta;
 
-        if (Time >= NextSpawnTime)
-        {
-            Spawn();
-        }
+        //if (Time >= NextSpawnTime)
+        //{
+        //    Spawn();
+        //}
     }
 
-    private void OnKillIncreased(object sender, OnKillEventArgs e)
+    //private void OnKillIncreased(object sender, OnKillEventArgs e)
+    //{
+    //    Difficulty = e.Kills / 10;
+    //}
+
+    private void OnEnemyWave(object sender, Wave wave)
     {
-        Difficulty = e.Kills / 10;
+        var spawnLocation = GetRandomOffMapLocation();
+
+        for (Int32 i = 0; i < wave.Count; i++)
+        {
+            SpawnSingle(SceneFromPath(wave.EnemyMix[(Int32)GD.Randi() % wave.EnemyMix.Count]), spawnLocation + GetSpawnAreaLocation());
+        }
     }
 
     private void Spawn()
     {
-        var SpawnLocation = GetRandomOffMapLocation();
-        if (Difficulty < 5)
-        {
-            for (Int32 i = (Int32)GD.Randi() % 2 + Difficulty; i > 0; i--)
-            {
-                SpawnSingle(Spider, SpawnLocation + GetSpawnAreaLocation());
-            }
+        //var SpawnLocation = GetRandomOffMapLocation();
+        //if (Difficulty < 5)
+        //{
+        //    for (Int32 i = (Int32)GD.Randi() % 2 + Difficulty; i > 0; i--)
+        //    {
+        //        SpawnSingle(Spider, SpawnLocation + GetSpawnAreaLocation());
+        //    }
 
-            NextSpawnTime = Time + 1f;
-        }
-        else //if (Difficulty < 10)
-        {
-            for (Int32 i = (Int32)GD.Randi() % 2 + Difficulty / 2; i > 0; i--)
-            {
-                SpawnSingle(Spider, SpawnLocation + GetSpawnAreaLocation());
-            }
-            for (Int32 i = (Int32)GD.Randi() % 1 + Difficulty / 4; i > 0; i--)
-            {
-                SpawnSingle(Car, SpawnLocation + GetSpawnAreaLocation());
-            }
-            NextSpawnTime = Time + 4f;
-        }
+        //    NextSpawnTime = Time + 1f;
+        //}
+        //else //if (Difficulty < 10)
+        //{
+        //    for (Int32 i = (Int32)GD.Randi() % 2 + Difficulty / 2; i > 0; i--)
+        //    {
+        //        SpawnSingle(Spider, SpawnLocation + GetSpawnAreaLocation());
+        //    }
+        //    for (Int32 i = (Int32)GD.Randi() % 1 + Difficulty / 4; i > 0; i--)
+        //    {
+        //        SpawnSingle(Car, SpawnLocation + GetSpawnAreaLocation());
+        //    }
+        //    NextSpawnTime = Time + 4f;
+        //}
     }
 
     private void SpawnSingle(PackedScene enemyScene, Vector2 location)
@@ -93,7 +118,7 @@ public class EnemySpawner : Node
         GetTree().Root.AddChild(enemy);
         enemy.Position = location;
         enemy.TargetLocation = Turret.GlobalPosition;
-        GD.Print($"spawned at {location}");
+        //GD.Print($"spawned at {location}");
     }
 
     private Vector2 GetRandomOffMapLocation() => new Vector2(0f, 1f).Rotated((Single)GD.RandRange(-Math.PI, Math.PI)) * 2500f;
