d39908fa1089581d696e0813acb744afc1079936
Adam Kauth <adamkauth@gmail.com>
2021-10-03 20:59:30 -0700

    Replace buttons

--- game/Scripts/SceneScripts/Instructions.cs
+++ game/Scripts/SceneScripts/Instructions.cs
@@ -5,11 +5,11 @@ public class Instructions : Node2D
 {
     const String BackPath = "Back";
 
-    public Button Back { get; private set; }
+    public TextureButton Back { get; private set; }
 
     public override void _Ready()
     {
-        Back = GetNode<Button>(BackPath);
+        Back = GetNode<TextureButton>(BackPath);
         Back.Connect("pressed", this, nameof(OnBackPressed));
     }
 
--- game/Scripts/SceneScripts/MainMenu.cs
+++ game/Scripts/SceneScripts/MainMenu.cs
@@ -12,17 +12,17 @@ public class MainMenu : Node2D
     const String CreditsPath = "Credits";
     const String ExitPath = "Exit";
 
-    public Button Play { get; private set; }
-    public Button Instructions { get; private set; }
-    public Button Credits { get; private set; }
-    public Button Exit { get; private set; } 
+    public TextureButton Play { get; private set; }
+    public TextureButton Instructions { get; private set; }
+    public TextureButton Credits { get; private set; }
+    public TextureButton Exit { get; private set; } 
 
     public override void _Ready()
     {
-        Play = GetNode<Button>(PlayPath);
-        Instructions = GetNode<Button>(InstructionsPath);
-        Credits = GetNode<Button>(CreditsPath);
-        Exit = GetNode<Button>(ExitPath);
+        Play = GetNode<TextureButton>(PlayPath);
+        Instructions = GetNode<TextureButton>(InstructionsPath);
+        Credits = GetNode<TextureButton>(CreditsPath);
+        Exit = GetNode<TextureButton>(ExitPath);
 
         Play.Connect("pressed", this, nameof(OnPlayPressed));
         Instructions.Connect("pressed", this, nameof(OnInstructionsPressed));
